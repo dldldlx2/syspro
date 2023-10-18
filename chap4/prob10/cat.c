aae51e46628903ec934d2f9d367c253950646359
dldldlx2 <kelly102525@gmail.com>
2023-10-18 07:17:53 +0000

    23.10.18

--- chap4/prob10/cat.c
+++ chap4/prob10/cat.c
@@ -1,20 +1,52 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#define MAXLINE 80
 
 int main(int argc, char *argv[]){
-        FILE *fp;
-        int c;
+    FILE *fp;
+    int c;
 	int i;
-for(i = 1; i < argc; i++){
-        if(argc < 2)
-                fp = stdin;
-        else fp = fopen(argv[1],"r");
+	
+	int line = 0;
+	char buffer[MAXLINE];
 
-        c = getc(fp);
-        while(c != EOF){
-                fputc(c, stdout);
-                c = fgetc(fp);
-        }
-        fclose(fp);
-}
-        return 0;
+	if (strcmp(argv[1], "-n") == 0){	
+        line++;
+		printf("%d ", line);
+		
+		if(argc < 2)
+			fp = stdin;
+		else
+			fp = fopen(argv[2], "r");
+
+		c = getc(fp);
+
+		while(c != EOF){
+			fputc(c, stdout);
+			c = fgetc(fp);
+		}
+
+		fclose(fp);
+	}
+	else{
+		for(i = 1; i < argc; i++){
+			
+			if(argc < 2)
+				fp = stdin;
+       		else
+				fp = fopen(argv[i],"r");
+
+       		c = getc(fp);
+
+        	while(c != EOF){
+             	  fputc(c, stdout);
+			  	 c = fgetc(fp);
+   			}
+
+       		fclose(fp);
+		}
+	}
+	
+	return 0;
 }
