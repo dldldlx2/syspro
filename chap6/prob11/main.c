8ecdd6f6e58ad96ba0b98d70797c23f08446e9dd
dldldlx2 <kelly102525@gmail.com>
2023-11-15 15:22:31 +0900

    23.11.15

--- chap6/prob11/main.c
+++ chap6/prob11/main.c
@@ -1,124 +1,130 @@
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <time.h>
-#include <grp.h>
-#include <pwd.h>
-#include <string.h> 
-#include <stdio.h>
-#include <stdlib.h>
-#include <dirent.h>
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<dirent.h>
+#include<time.h>
+#include<pwd.h>
+#include<grp.h>
+#include<stdlib.h>
+#include<string.h>
 
 char type(mode_t);
-char *perm(mode_t);
+char* perm(mode_t);
 void printStat(char*, char*, struct stat*);
+void InodeprintStat(char*, char*, struct stat*);
+void op_PprintStat(char*, char*, struct stat*);
+void op_QprintStat(char*, char*, struct stat*);
 
-void listDir(char *dir, int show_hidden, int show_details, int recursive);
+int main(int argc, char *argv[]) {
+	DIR *dp;
+	char *dir;
+	struct dirent *d;
+	struct stat st;
+	char path[BUFSIZ + 1];
 
-int main(int argc, char **argv)
-{
-    char *dir = ".";
-    int show_hidden = 0;
-    int show_details = 0;
-    int recursive = 0;
+	dir = ".";
 
-    for (int i = 1; i < argc; ++i) {
-        if (strcmp(argv[i], "-a") == 0) {
-            show_hidden = 1;
-        } else if (strcmp(argv[i], "-l") == 0) {
-            show_details = 1;
-        } else if (strcmp(argv[i], "-R") == 0) {
-            recursive = 1;
-        } else {
-            dir = argv[i];
-        }
-    }
-
-    listDir(dir, show_hidden, show_details, recursive);
-
-    return 0;
+	if((dp = opendir(dir)) < 0) {
+		perror(dir);
+		exit(1);
+	}
+	while((d = readdir(dp)) != NULL) {
+		sprintf(path, "%s/%s", dir, d -> d_name);
+		if((lstat(path, &st)) < 0) {
+			perror(path);
+		}
+		else if(argc >= 2) {
+			if((strcmp(argv[1], "-i")) == 0) {
+				InodeprintStat(path, d -> d_name, &st);
+			}
+			else if((strcmp(argv[1], "-p")) == 0) {
+				op_PprintStat(path, d -> d_name, &st);
+			}
+			else if((strcmp(argv[1], "-Q")) == 0) {
+				op_QprintStat(path, d -> d_name, &st);
+			}
+		}
+		else {
+			printStat(path, d -> d_name, &st);
+		}
+	}
+	closedir(dp);
+	exit(0);
 }
-
-void listDir(char *dir, int show_hidden, int show_details, int recursive)
-{
-    DIR *dp;
-    struct stat st;
-    struct dirent *d;
-    char path[BUFSIZ + 1];
-
-    if ((dp = opendir(dir)) == NULL) {
-        perror(dir);
-        exit(EXIT_FAILURE);
-    }
-
-    while ((d = readdir(dp)) != NULL) {
-        if (!show_hidden && d->d_name[0] == '.') {
-            continue;
-        }
-
-        sprintf(path, "%s/%s", dir, d->d_name);
-
-        if (lstat(path, &st) < 0) {
-            perror(path);
-        } else {
-            if (show_details) {
-                printStat(path, d->d_name, &st);
-            } else {
-                printf("%s\n", d->d_name);
-            }
-
-            if (recursive && S_ISDIR(st.st_mode) && strcmp(d->d_name, ".") != 0 && strcmp(d->d_name, "..") != 0) {
-                printf("\n%s:\n", path);
-                listDir(path, show_hidden, show_details, recursive);
-            }
-        }
-    }
-
-    closedir(dp);
+char type(mode_t mode) {
+	if(S_ISREG(mode)) {
+		return '-';
+	}
+	if(S_ISDIR(mode)) {
+		return 'd';
+	}
+	if(S_ISCHR(mode)) {
+		return 'c';
+	}
+	if(S_ISBLK(mode)) {
+		return 'b';
+	}
+	if(S_ISLNK(mode)) {
+		return 'l';
+	}
+	if(S_ISFIFO(mode)) {
+		return 'p';
+	}
+	if(S_ISSOCK(mode)) {
+		return 's';
+	}
 }
+char* perm(mode_t mode) {
+	static char perms[10];
+	strcpy(perms, "---------");
 
-void printStat(char *pathname, char *file, struct stat *st)
-{
-    printf("%5d ", st->st_blocks);
-    printf("%c%s ", type(st->st_mode), perm(st->st_mode));
-    printf("%3d ", st->st_nlink);
-    printf("%s %s ", getpwuid(st->st_uid)->pw_name, getgrgid(st->st_gid)->gr_name);
-    printf("%9d ", st->st_size);
-    printf("%.12s ", ctime(&st->st_mtime) + 4);
-    printf("%s\n", file);
+	for(int i = 0; i < 3; i ++) {
+		if(mode & (S_IRUSR >> i * 3)) {
+			perms[i * 3] = 'r';
+		}
+		if(mode & (S_IWUSR >> i * 3)) {
+			perms[i * 3 + 1] = 'w';
+		}
+		if(mode & (S_IXUSR >> i * 3)) {
+			perms[i * 3 + 2] = 'x';
+		}
+	}
+	return perms;
 }
-
-char type(mode_t mode)
-{
-    if (S_ISREG(mode))
-        return('-');
-    if (S_ISDIR(mode))
-        return('d');
-    if (S_ISCHR(mode))
-        return('c');
-    if (S_ISBLK(mode))
-        return('b');
-    if (S_ISLNK(mode))
-        return('l');
-    if (S_ISFIFO(mode))
-        return('p');
-    if (S_ISSOCK(mode))
-        return('s');
-
-    return('?');
+void printStat(char *pathname, char *file, struct stat *st) {
+	printf("%5ld ", st -> st_blocks);
+	printf("%c%s ", type(st -> st_mode), perm(st -> st_mode));
+	printf("%3d ", st -> st_nlink);
+	printf("%s %s ", getpwuid(st -> st_uid) -> pw_name, getgrgid(st -> st_uid) -> gr_name);
+	printf("%9ld ", st -> st_size);
+	printf("%.12s ", ctime(&st -> st_mtime) + 4);
+	printf("%s\n", file);
 }
-
-char* perm(mode_t mode)
-{
-    static char perms[10];
-    strcpy(perms, "---------");
-
-    for (int i = 0; i < 3; i++) {
-        if (mode & (S_IRUSR >> i * 3))
-            perms[i * 3] = 'r';
-        if (mode & (S_IWUSR >> i * 3))
-            perms[i * 3 + 1] = 'w';
-        if (mode & (S_IXUSR >> i * 3))
-            perms[i * 3 + 2] = 'x';
-    }
-    return(perms);
+void InodeprintStat(char *pathname, char *file, struct stat *st) {
+	printf("%5ld ", st -> st_blocks);
+	printf("%5ld ", st -> st_ino);
+	printf("%c%s ", type(st -> st_mode), perm(st -> st_mode));
+	printf("%3d ", st -> st_nlink);
+	printf("%s %s ", getpwuid(st -> st_uid) -> pw_name, getgrgid(st -> st_uid) -> gr_name);
+	printf("%9ld ", st -> st_size);
+	printf("%.12s ", ctime(&st -> st_mtime) + 4);
+	printf("%s\n", file);
+}
+void op_PprintStat(char *pathname, char *file, struct stat *st) {
+	printf("%5ld ", st -> st_blocks);
+	printf("%c%s ", type(st -> st_mode), perm(st -> st_mode));
+	printf("%3d ", st -> st_nlink);
+	printf("%s %s ", getpwuid(st -> st_uid) -> pw_name, getgrgid(st -> st_uid) -> gr_name);
+	printf("%9ld ", st -> st_size);
+	printf("%.12s ", ctime(&st -> st_mtime) + 4);
+	printf("%s/\n", file);
+}
+void op_QprintStat(char *pathname, char *file, struct stat *st) {
+	printf("%5ld ", st -> st_blocks);
+	printf("%c%s ", type(st -> st_mode), perm(st -> st_mode));
+	printf("%3d ", st -> st_nlink);
+	printf("%s %s ", getpwuid(st -> st_uid) -> pw_name, getgrgid(st -> st_uid) -> gr_name);
+	printf("%9ld ", st -> st_size);
+	printf("%.12s ", ctime(&st -> st_mtime) + 4);
+	printf("\"%s\"\n", file);
 }
