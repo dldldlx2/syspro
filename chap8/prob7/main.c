a3dc9097486b28360f7f9df5c2721994b4c6f1bf
dldldlx2 <kelly102525@gmail.com>
2023-11-29 05:56:52 +0000

    23.11.2923.11.2923.11.2923.11.2923.11.2923.11.2923.11.2923.11.2923.11.2923.11.2923.11.2923.11.2923.11.2923.11.2923.11.2923.11.2923.11.2923.11.2923.11.2923.11.2923.11.2923.11.2923.11.29

--- chap8/prob7/main.c
+++ chap8/prob7/main.c
@@ -4,13 +4,13 @@
 #include <unistd.h>
 #include <pwd.h>
 #include <grp.h>
-
 int main(int argc, char **argv) {
-        extern char **environ;
-
-        if((strcmp(argv[1], "-e")) == 0) {
-                if(argv[2] != NULL) {
-                        char *output = getenv(argv[2]);
+	extern char **environ;
+	if((strcmp(argv[1], "-e")) == 0) {
+		if(argv[2] != NULL) {
+			char *output = getenv(argv[2]);
+			if(output != NULL)
+				printf("%s\n", output);
 		}
 		else {
 			while(*environ != 0) {
@@ -19,19 +19,19 @@ int main(int argc, char **argv) {
 			}
 		}
 	}
-        else if((strcmp(argv[1], "-u")) == 0) {
-                printf("My Realistic User ID : %d(%s)\n", getuid(), getpwuid(getuid()) -> pw_name);
-                printf("My Valid User ID : %d(%s)\n", geteuid(), getpwuid(geteuid()) -> pw_name);
-        }
-        else if((strcmp(argv[1], "-g")) == 0) {
-                printf("My Realistic Group ID : %d(%s)\n", getgid(), getgrgid(getgid()) -> gr_name);
-                printf("My Valid Group ID : %d(%s)\n", getegid(), getgrgid(getegid()) -> gr_name);
-        }
-        else if((strcmp(argv[1], "-i")) == 0) {
-                printf("my process number : [%d]\n", getpid());
-        }
-        else if((strcmp(argv[1], "-p")) == 0) {
-                printf("my parent's process Number : [%d]\n", getppid());
-        }
-        exit(0);
+	else if((strcmp(argv[1], "-u")) == 0) {
+		printf("My Realistic User ID : %d(%s)\n", getuid(), getpwuid(getuid()) -> pw_name);
+		printf("My Valid User ID : %d(%s)\n", geteuid(), getpwuid(geteuid()) -> pw_name);
+	}
+	else if((strcmp(argv[1], "-g")) == 0) {
+		printf("My Realistic Group ID : %d(%s)\n", getgid(), getgrgid(getgid()) -> gr_name);
+		printf("My Valid Group ID : %d(%s)\n", getegid(), getgrgid(getegid()) -> gr_name);
+	}
+	else if((strcmp(argv[1], "-i")) == 0) {
+		printf("my process number : [%d]\n", getpid());
+	}
+	else if((strcmp(argv[1], "-p")) == 0) {
+		printf("my parent's process Number : [%d]\n", getppid());
+	}
+	exit(0);
 }
